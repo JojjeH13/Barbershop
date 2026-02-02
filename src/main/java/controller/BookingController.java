4b4408a57243435d21a50c75a8ae19e852334f50
badmah <badranmody@gmail.com>
2026-02-02 13:34:48 +0100

    senaste

--- src/main/java/controller/BookingController.java
+++ src/main/java/controller/BookingController.java
@@ -1,4 +1,23 @@
 package controller;
 
+import entity.Booking;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import repository.BookingRepository;
+
+@RestController
+@RequestMapping("/bookings")
 public class BookingController {
+
+    private final BookingRepository repo;
+
+    public BookingController(BookingRepository repo) {
+
+    }
+    @PostMapping
+    public Booking create(@RequestBody Booking booking) {
+        return repo.save(booking);
+    }
 }
--- src/main/java/repository/BookingRepository.java
+++ src/main/java/repository/BookingRepository.java
@@ -1,4 +1,9 @@
 package repository;
 
+import entity.Booking;
+
 public class BookingRepository {
+    public Booking save(Booking booking) {
+
+    }
 }
