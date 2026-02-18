a830d59620c6873ea408ff77461029d7790a1571
filipobradovic <filip01obradovic@gmail.com>
2026-02-18 12:42:16 +0100

    Booking shaize

--- src/main/java/entity/Booking.java
+++ src/main/java/entity/Booking.java
@@ -1,11 +1,31 @@
 package entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 @Entity
 public class Booking {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bookingid;
+
+    @ManyToOne
+    private Customer customer;
+
+    @ManyToOne
+    private Employee employee;
+
+    @ManyToOne
+    private Product product;
+
+    private LocalDate bookingDate;
+    private LocalTime bookingTime;
+
+    @Enumerated(EnumType.STRING)
+    private Enum.Status status;
 
 }
