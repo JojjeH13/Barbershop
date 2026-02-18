8e175e028a7e44c864f0122fa68f60fa8ec206e3
filipobradovic <filip01obradovic@gmail.com>
2026-02-18 11:49:41 +0100

    New classes is entity package again

--- src/main/java/entity/Employee.java
+++ src/main/java/entity/Employee.java
@@ -1,5 +1,8 @@
 package entity;
 
+import jakarta.persistence.Entity;
+
+@Entity
 public class Employee {
     String mahmoud = "lol";
 }
--- src/main/java/entity/Product.java
+++ src/main/java/entity/Product.java
@@ -1,5 +1,8 @@
 package entity;
 
+import jakarta.persistence.Entity;
+
+@Entity
 public class Product {
     String denis = "lol igen";
 }
