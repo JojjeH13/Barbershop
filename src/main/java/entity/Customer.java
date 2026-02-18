f31ab3a66bb17937c027416996ab326ade1d7105
badmah <badranmody@gmail.com>
2026-02-18 12:42:50 +0100

    GetMapping List

---
+++ src/main/java/entity/Customer.java
@@ -0,0 +1,52 @@
+package entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long customerid;
+
+    private String customerName;
+    private String customerMail;
+    private int customerPhone;
+
+    public Long getCustomerid() {
+        return customerid;
+    }
+
+    public void setCustomerid(Long customerid) {
+        this.customerid = customerid;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerMail() {
+        return customerMail;
+    }
+
+    public void setCustomerMail(String customerMail) {
+        this.customerMail = customerMail;
+    }
+
+
+    public int getCustomerPhone() {
+        return customerPhone;
+    }
+
+    public void setCustomerPhone(int customerPhone) {
+        this.customerPhone = customerPhone;
+    }
+
+}
