cf99b3808bf0079a4330dc9e68cf69649c0ac2d4
badmah <badranmody@gmail.com>
2026-03-04 12:54:08 +0100

    GetMapping List

--- src/main/java/com/example/barbershop/controller/ProductController.java
+++ src/main/java/com/example/barbershop/controller/ProductController.java
@@ -1,8 +1,13 @@
 package com.example.barbershop.controller;
 
+import com.example.barbershop.entity.Customer;
+import com.example.barbershop.entity.Product;
 import com.example.barbershop.service.ProductService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/Products")
@@ -12,6 +17,11 @@ public class ProductController {
 
     public ProductController(ProductService productService) {
         this.productService = productService;
+    }
+
+    @GetMapping
+    public List<Product> getAllCustomers() {
+        return productService.getAllProducts();
     }
 
 }
