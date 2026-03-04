4a30aa4293746004ef24c5c69ac584369714fdaf
denisraama <denisraama@gmail.com>
2026-03-04 12:50:49 +0100

    Started with ProductController

--- src/main/java/com/example/barbershop/controller/ProductController.java
+++ src/main/java/com/example/barbershop/controller/ProductController.java
@@ -1,4 +1,17 @@
 package com.example.barbershop.controller;
 
+import com.example.barbershop.service.ProductService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/Products")
 public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
 }
