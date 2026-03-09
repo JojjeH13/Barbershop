dc93ff43bb7b78ecdd72227d38720fe7fa3f9b35
badmah <badranmody@gmail.com>
2026-03-09 12:52:46 +0100

    GetMapping List

--- src/main/java/com/example/barbershop/controller/ProductController.java
+++ src/main/java/com/example/barbershop/controller/ProductController.java
@@ -1,6 +1,5 @@
 package com.example.barbershop.controller;
 
-import com.example.barbershop.entity.Customer;
 import com.example.barbershop.entity.Product;
 import com.example.barbershop.service.ProductService;
 import org.springframework.web.bind.annotation.*;
